3c1ac6e61e48cb600a1b615ead468dbc9df6d069
thejaswinipathi <thejaswini_pathi@yahoo.co.in>
2020-03-18 10:26:02 +0530

    Temp

--- book/views.py
+++ book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view(['POST'])
+def query_book(request):
+    if request.method == 'POST':
+        print("request.data")
+        print(request.data)
