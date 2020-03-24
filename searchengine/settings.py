733af0befedd33a71a76637adadc4d86268e32bd
thejaswinipathi <thejaswini_pathi@yahoo.co.in>
2020-03-24 13:51:24 +0530

    changes in settings file

--- searchengine/settings.py
+++ searchengine/settings.py
@@ -40,7 +40,12 @@ INSTALLED_APPS = [
     'book',
     'rest_framework'
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
